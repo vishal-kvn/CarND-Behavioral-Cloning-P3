16a050d43d279316874de0b186c497ca7c89f054
vkajjam <vishal.kvn@gmail.com>
2018-12-12 11:25:37 +0000

    Model weights for Lenet arch

--- clone.py
+++ clone.py
@@ -30,7 +30,10 @@ y_train = np.array(measurements)
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 
+"""
 model = Sequential()
 # Basic architecture
 model.add(Lambda(lambda x: x/255.0 -0.5, input_shape=(160, 320, 3)))
@@ -41,9 +44,43 @@ model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
 
 model.save('model.h5')
+"""
 
-# python clone.py
-"""
 # LeNet architecture
+model = Sequential()
 model.add(Lambda(lambda x: x/255.0 -0.5, input_shape=(160, 320, 3)))
-"""
+
+#Layer 1
+#Conv Layer 1
+model.add(Conv2D(filters = 6, 
+                 kernel_size = 5, 
+                 strides = 1, 
+                 activation = 'relu', 
+                 input_shape = (160,320,3)))
+#Pooling layer 1
+model.add(MaxPooling2D(pool_size = 2, strides = 2))
+#Layer 2
+#Conv Layer 2
+model.add(Conv2D(filters = 16, 
+                 kernel_size = 5,
+                 strides = 1,
+                 activation = 'relu',
+                 input_shape = (14,14,6)))
+#Pooling Layer 2
+model.add(MaxPooling2D(pool_size = 2, strides = 2))
+#Flatten
+model.add(Flatten())
+#Layer 3
+#Fully connected layer 1
+model.add(Dense(units = 120))
+#Layer 4
+#Fully connected layer 2
+model.add(Dense(units = 84))
+#Layer 5
+#Output Layer
+model.add(Dense(units = 1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+
+model.save('model.h5')
