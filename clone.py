56f30bc90a2a78b1c7b70b9eea17c489a6d101bc
vkajjam <vishal.kvn@gmail.com>
2018-12-14 01:03:45 +0000

    Include left, right images and image cropping

--- clone.py
+++ clone.py
@@ -15,21 +15,37 @@ images = []
 measurements = []
 
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    # current_path = '../data/IMG/' + filename
-    current_path = '/opt/data/IMG/' + filename
-    image = cv2.imread(current_path)
-    #image = ndimage.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
+    steering_center = float(line[3])
+    correction = 0.2
+    steering_left = steering_center + correction
+    steering_right = steering_center - correction
+    
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        # current_path = '../data/IMG/' + filename
+        current_path = '/opt/data/IMG/' + filename
+        image = cv2.imread(current_path)
+        #image = ndimage.imread(current_path)
+        images.append(image)
+     
+    measurements.append(steering_center)
+    measurements.append(steering_left)
+    measurements.append(steering_right)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement*-1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda
+from keras.layers import Cropping2D
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 
@@ -49,6 +65,7 @@ model.save('model.h5')
 # LeNet architecture
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 -0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,20), (0,0)), input_shape=(3,160,320)))
 
 #Layer 1
 #Conv Layer 1
@@ -56,7 +73,7 @@ model.add(Conv2D(filters = 6,
                  kernel_size = 5, 
                  strides = 1, 
                  activation = 'relu', 
-                 input_shape = (160,320,3)))
+                 input_shape = (70,320,3)))
 #Pooling layer 1
 model.add(MaxPooling2D(pool_size = 2, strides = 2))
 #Layer 2
